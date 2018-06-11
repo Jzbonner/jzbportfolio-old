39c4c90166b98c9bd5782315fb5fe12cf460a9a5
Jzbonner <jarrett.bonner@gmail.com>
2018-06-11 19:56:17 -0400

    changes on test page branch for new layout with functionality

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,136 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import Sidebar from './Sidebar';
-import Viewport from './Viewport';
+import Bio from './Bio';
+import Home from './Home';
+import Experience from './Experience';
+import Resume from './Resume'; 
+import Portfolio from './Portfolio'; 
+import Footer from './Footer'; 
+
 import '../App.css';
+import NavIndex from '../assets/source-code.png';
+
+const routes = [
+    {
+        path : "/", 
+        exact: true, 
+        sidebar: () => <Sidebar />,
+        main: () => <Home />,
+    }, 
+    {
+        path : "/Home", 
+        exact: true, 
+        sidebar: () => <Sidebar />,
+        main: () => <Home />,
+    }, 
+    {
+        path : "/Bio", 
+        exact: true, 
+        sidebar: () => <Sidebar />,
+        main: () => <Bio />,
+    },
+    {
+        path : "/Experience", 
+        exact: true, 
+        sidebar: () => <Sidebar />,
+        main: () => <Experience />,
+    }, 
+    {
+        path : "/Portfolio", 
+        exact: true, 
+        sidebar: () => <Sidebar />,
+        main: () => <Portfolio />,
+    },
+    {
+        path : "/Resume", 
+        exact: true, 
+        sidebar: () => <Sidebar />,
+        main: () => <Resume />,
+    },  
+]; 
 
 export default class App extends Component {
     render() {
         return (
-            <div className="container-fluid">
-                <div className="row">
-                    <Sidebar />
-                    <Viewport />
-                </div>
+            <Router>
+            {/*
+                Section for displaying the navbar/sidebar links to display the 
+                actual content; this section needs to be bootstrapped to provide 
+                more of a grid layout for the UI of the page
+
+                Also update the CSS file to actually work in tandem with the new layout
+                Should have the navbar/sidebar be one feature that is responsive to 
+                smaller displays via media queries. 
+
+            */}
+
+                <div>
+                    <nav className="navbar navbar-expand-md navbar-light">
+                    <a className="navbar-brand" href="https://jzb-co.surge.sh">
+                        <img src={NavIndex} className="nav-index" alt="nav-index" />
+                    </a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapseItems" aria-controls="navbarCollapseItems" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarCollapseItems">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                            <Link to="/Home" className="nav-link">
+                                Home
+                            </Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link to="/Bio" className="nav-link">
+                                Bio
+                            </Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link to="/Experience" className="nav-link">
+                                Experience
+                            </Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link to="/Portfolio" className="nav-link">
+                                Portfolio
+                            </Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link to="/Resume" className="nav-link">
+                                Resume
+                            </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    </nav>
+                
+
+                {routes.map((route, index) => (
+                    <Route
+                        key={index}
+                        path={route.path}
+                        exact={route.exact}
+                        component={route.sidebar}
+                    />
+                ))}
+            
+            {/*
+                Section for displaying the next actual content that is triggered 
+                from the navbar/sidebar sections 
+            */}
+
+            <div>
+                {routes.map((route, index) => (
+                    <Route
+                        key={index}
+                        path={route.path}
+                        exact={route.exact}
+                        component={route.main}
+                    />
+                ))}
             </div>
-        );
-    }
-}
+
+        </div>
+  </Router>
+)}};
--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import App from './containers/App';
 
 
 export default (
     <Router>
-        <Switch>
-            <Route path="/" exact component={App} />
-            <Route path="/Bio" exact component={App} /> 
-
-            {/*
-                
-                The below routes do keep track of the browser URL 
-                but they do not render the correct components on their own. 
-                Look into how to handle that error so when someone reloads the 
-                page on that specific url specification it loads the correct 
-                component class. 
-
-            */}
-            
-            <Route path="/Portfolio" component={App} />
-            <Route path="/Experience" exact component={App} />
-            <Route path="/Resume" exact component={App} />
-            
-        </Switch> 
+        <Route path="/" exact component={App} />  
     </Router>
 )
