8f86e8fe57d8e3820de8f7dd7a5fe65839f7694d
Jzbonner <jarrett.bonner@gmail.com>
2018-06-26 23:36:36 -0400

    changes to layout

--- src/containers/Bio.js
+++ src/containers/Bio.js
@@ -9,18 +9,58 @@ export default class Bio extends Component {
             <div className="bio"> 
                 <div className="row">
                     <div className="col-lg-12">
-                        <p> 
-                            Educational Background - Name of High School; Extracurricular Activities; Freelance Beginnings
-                            College Career - University of North Carolina (Bachelor's of Science in Information Science)
-                            College Course Listing - INLS Courses that relate to Information and Computer Science
-                            Skills Developed in College - Intermediate Spanish; Office Suite Proficiency; Volunteering; Coordinating an Internet Presence 
-                            General Interest - Kaggle Data Science Competition, Hacker Rank Competition, Independent App Development, Quantitative Analysis and Quantum Computing 
+                        <p className="general-interest"> 
+                        Aside from defining myself academically, I have always strived to be a well rounded person 
+                        in both extracurricular activities as well as those that would further my academic career. 
+                        My interest in Computer and Data Science grew from the course work I was able to complete 
+                        during my undergraduate career. 
                         </p> 
 
-                        <p> 
-                            Internship Background - Small blurb about Internships held in the past and connections made 
-                            Employment Background - Small Blurb about Jobs held in the past and connections made
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>College Career - University of North Carolina 
+                                    (Bachelor's of Science in Information Science)</td> 
+                                </tr> 
+                                <tr>
+                                    <td> Course 1 </td>
+                                    <td> Info on Course </td>
+                                </tr>
+                                <tr>
+                                    <td> Course 2 </td>
+                                    <td> Info on Course </td>
+                                </tr>
+                                <tr>
+                                    <td>Course 3</td>
+                                    <td> Info on Course </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <p className="general-interest-2">
+                        Gaining experience in life is about finding your medium in a world of extremes. I have often 
+                        found direction by volunteering my time to aid in community organized projects and services. 
+
+                        {/* 
+                        
+                            Create an unorganized list of all the volunteer work and community 
+                            service work from resume 
+
+                        */}
+
+                        {/* Block Quote the Following */}
+                        General Interest - Intermediate Spanish, Kaggle Data Science Competition, Hacker Rank Competition, 
+                        Independent App Development, Quantitative Analysis and Quantum Computing 
                         </p> 
+
+                        <p className="internship"> 
+                        Internship Background - Small blurb about Internships held in the past and connections made 
+                        </p> 
+
+                        <p className="employment">
+                        Employment Background - Small Blurb about Jobs held in the past and connections made
+                        </p>
+
                     </div>
                 </div> 
                 <Footer />
--- src/containers/Experience.js
+++ src/containers/Experience.js
@@ -7,33 +7,32 @@ export default class Experience extends Component {
     render() {
         return (
             <div className="experience">
+                <h2> Employment Experience </h2>
+                <br />
                 <p>
-                Monster Marketers Inc. Information Technology Consultant/ Customer Service Representative Lawrenceville, GA, May 2014 – January 2015 
-                •	Served customers by providing relevant product and advertising service information
-                •	Maintained and updated customer records and financial accounts 
-                •	Developed interpersonal skills by communicating with customer clientele and company administration
-                •	Gained proficiency in an office setting as well as expertise in Microsoft Office 
-                <br /><br />
-                Office of Arts and Sciences Information Services (OASIS) WordPress Technician  
-                UNC – Chapel Hill, October 2012 – May 2013
-                •	Worked as a WordPress technician providing technology solutions and troubleshooting assistance services 
-                •	Worked with UNC faculty and administration to build technological connections throughout the on-campus community  
-                •	Gained proficient computer skills in HTML, WordPress, and Python; and enhanced my task management abilities in a professional work setting 
-                <br /><br />
-                Cisco Systems Inc. Junior and Senior Intern 
-                Lawrenceville, GA, October 2010 - April 2012
-                •	Worked as a materials testing assistant in the mechanical engineering department
-                •	Heat tested a number of different devices from set top boxes to wireless routers and modems
-                •	Gained a fundamental understanding of IT security and SQL environments  such as Microsoft Access and MySQL
-                •	Gained knowledge on how to work with a team, how to lead a team, and the benefits and disadvantages of working solo versus in a group setting
-                <br /><br />
-                Flood Atlanta Sub-Contracting Company Employee 
-                Lawrenceville, GA; August 2012 – October 2013
-                •	Worked with a team to restore and renovate water-damaged properties
-                •	Worked with information databases for the purpose of storing details on company projects, customer records and financial documentation
-                •	Worked in a supporting role to the company and lead in organizing general labor projects 
-                •	Gained fundamental construction and customer care/relations experience 
-                •	Gained valuable experience with food preparation and food service via volunteering at local hospitals                 
+                Monster Marketers Inc. IT Consultant/ Customer Service Representative Lawrenceville, GA, <br /> 
+                •	Served customers by providing relevant product and advertising service information <br /> 
+                •	Maintained and updated customer records and financial accounts <br /> 
+                •	Developed interpersonal skills by communicating with customer clientele and company administration <br /> 
+                •	Gained proficiency in an office setting as well as expertise in Microsoft Office <br />  
+                <br />
+                Office of Arts and Sciences Information Services (OASIS) WordPress Technician Chapel Hill, NC <br /> 
+                •	Worked as a WordPress technician providing technology solutions and troubleshooting assistance services <br /> 
+                •	Worked with UNC faculty and administration to build technological connections throughout the on-campus community  <br /> 
+                •	Gained proficient computer skills in HTML, WordPress, and Python; and enhanced my task management abilities in a professional work setting <br /> 
+                <br />
+                Cisco Systems Inc. Junior and Senior Intern Lawrenceville, GA <br /> 
+                •	Worked as a materials testing assistant in the mechanical engineering department <br /> 
+                •	Heat tested a number of different devices from set top boxes to wireless routers and modems <br /> 
+                •	Gained a fundamental understanding of IT security and SQL environments  such as Microsoft Access and MySQL <br /> 
+                •	Gained knowledge on how to work with a team, how to lead a team, and the benefits and disadvantages of working solo versus in a group setting <br /> 
+                <br />
+                Flood Atlanta Sub-Contracting Company Employee Lawrenceville, GA <br /> 
+                •	Worked with a team to restore and renovate water-damaged properties <br /> 
+                •	Worked with information databases for the purpose of storing details on company projects, customer records and financial documentation <br /> 
+                •	Worked in a supporting role to the company and lead in organizing general labor projects <br /> 
+                •	Gained fundamental construction and customer care/relations experience <br /> 
+                •	Gained valuable experience with food preparation and food service via volunteering at local hospitals <br />              
                 </p>
                 <Footer />
             </div> 
