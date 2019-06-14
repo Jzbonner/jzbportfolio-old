9b122be66e09dfbacdebb00f95d908b565b814c9
Jzbonner <jarrett.bonner@gmail.com>
2019-06-14 01:05:41 -0400

    changes to format of app component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,136 +1,167 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+/* Pass prop value from the App.js constructor to the Navbar component
+ * in order to modularize the layout of the application. Use state
+ * management in order to properly set and get parameters. 
+ *
+ * 1. Create Main App constructor {set parameters}
+ * 2. Create Navbar component 
+ * 3. Set scaffold (/collapse) to default and toggle onClick 
+ * */
+
+
+
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 import Sidebar from './Sidebar';
 import Bio from './Bio';
 import Home from './Home';
 import Experience from './Experience';
-import Resume from './Resume'; 
-import Portfolio from './Portfolio'; 
+import Resume from './Resume';
+import Portfolio from './Portfolio';
 
 import NavIndex from '../assets/source-code.png';
 import '../App.css';
 
-
 const routes = [
-    {
-        path : "/", 
-        exact: true, 
-        sidebar: () => <Sidebar />,
-        main: () => <Home />,
-    }, 
-    {
-        path : "/Home", 
-        exact: true, 
-        sidebar: () => <Sidebar />,
-        main: () => <Home />,
-    }, 
-    {
-        path : "/Bio", 
-        exact: true, 
-        sidebar: () => <Sidebar />,
-        main: () => <Bio />,
-    },
-    {
-        path : "/Experience", 
-        exact: true, 
-        sidebar: () => <Sidebar />,
-        main: () => <Experience />,
-    }, 
-    {
-        path : "/Portfolio", 
-        exact: true, 
-        sidebar: () => <Sidebar />,
-        main: () => <Portfolio />,
-    },
-    {
-        path : "/Resume", 
-        exact: true, 
-        sidebar: () => <Sidebar />,
-        main: () => <Resume />,
-    },  
-]; 
+  {
+    path: '/',
+    exact: true,
+    sidebar: () => <Sidebar />,
+    main: () => <Home />,
+  },
+  {
+    path: '/Home',
+    exact: true,
+    sidebar: () => <Sidebar />,
+    main: () => <Home />,
+  },
+  {
+    path: '/Bio',
+    exact: true,
+    sidebar: () => <Sidebar />,
+    main: () => <Bio />,
+  },
+  {
+    path: '/Experience',
+    exact: true,
+    sidebar: () => <Sidebar />,
+    main: () => <Experience />,
+  },
+  {
+    path: '/Portfolio',
+    exact: true,
+    sidebar: () => <Sidebar />,
+    main: () => <Portfolio />,
+  },
+  {
+    path: '/Resume',
+    exact: true,
+    sidebar: () => <Sidebar />,
+    main: () => <Resume />,
+  },
+];
 
 export default class App extends Component {
-    render() {
-        return (
-        <Router>
-            <div className="container-fluid">
-                <div className="row design-bg">
-                    {/*
+  render() {
+    return (
+      <Router>
+        <div className="container-fluid">
+          <div className="row design-bg">
+            {/*
                         Navbar Section with Link and BrowserRouter functionality 
                     */}
-                    <div className="col-lg-2">
-                        <nav className="navbar navbar-light">
-                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapseItems" aria-controls="navbarCollapseItems" aria-expanded="false" aria-label="Toggle navigation">
-                                <img src={NavIndex} className="nav-index" alt="nav-index" />
-                            </button>
-                            <div className="collapse navbar-collapse" id="navbarCollapseItems">
-                                <ul className="navbar-nav mr-auto">
-                                    <li className="nav-item">
-                                        <Link to="/Home" className="nav-link">
-                                        <p className="nav-id"><i id="icon" className="fas fa-circle-notch"></i> Home </p>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/Bio" className="nav-link">
-                                        <p className="nav-id"><i id="icon" className="far fa-user-circle"></i> Bio </p> 
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/Experience" className="nav-link">
-                                        <p className="nav-id"><i id="icon" className="fas fa-flask"></i> Experience </p> 
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/Portfolio" className="nav-link">
-                                        <p className="nav-id"><i id="icon" className="far fa-file-alt"></i> Portfolio </p> 
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link to="/Resume" className="nav-link">
-                                        <p className="nav-id"><i id="icon" className="fas fa-align-right"></i> Resume </p> 
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </nav>
-                    </div>
-                    {/*
+            <div className="col-lg-2">
+              <nav className="navbar navbar-light">
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbarCollapseItems"
+                  aria-controls="navbarCollapseItems"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation">
+                  <img src={NavIndex} className="nav-index" alt="nav-index" />
+                </button>
+                <div
+                  className="collapse navbar-collapse"
+                  id="navbarCollapseItems">
+                  <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                      <Link to="/Home" className="nav-link">
+                        <p className="nav-id">
+                          <i id="icon" className="fas fa-circle-notch"></i> Home{' '}
+                        </p>
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/Bio" className="nav-link">
+                        <p className="nav-id">
+                          <i id="icon" className="far fa-user-circle"></i> Bio{' '}
+                        </p>
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/Experience" className="nav-link">
+                        <p className="nav-id">
+                          <i id="icon" className="fas fa-flask"></i> Experience{' '}
+                        </p>
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/Portfolio" className="nav-link">
+                        <p className="nav-id">
+                          <i id="icon" className="far fa-file-alt"></i>{' '}
+                          Portfolio{' '}
+                        </p>
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/Resume" className="nav-link">
+                        <p className="nav-id">
+                          <i id="icon" className="fas fa-align-right"></i>{' '}
+                          Resume{' '}
+                        </p>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+            {/*
                         Functionality layout for displaying the navbar/sidebar links to 
                         show the page content
                     */}
-                    <div className="col-lg-3">
-                        <div className="sidebar-view">
-                        {routes.map((route, index) => (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                exact={route.exact}
-                                component={route.sidebar}
-                            />
-                        ))}
-                        </div>
-                    </div>
-                    {/*
+            <div className="col-lg-3">
+              <div className="sidebar-view">
+                {routes.map((route, index) => (
+                  <Route
+                    key={index}
+                    path={route.path}
+                    exact={route.exact}
+                    component={route.sidebar}
+                  />
+                ))}
+              </div>
+            </div>
+            {/*
                         Section for displaying the actual content that is triggered 
                         from the navbar/sidebar sections 
                     */}
-                    <div className="col-lg-7">
-                        <div className="content-view">
-                        {routes.map((route, index) => (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                exact={route.exact}
-                                component={route.main}
-                            />
-                        ))}
-                        </div>
-                    </div>
-                </div> 
+            <div className="col-lg-7">
+              <div className="content-view">
+                {routes.map((route, index) => (
+                  <Route
+                    key={index}
+                    path={route.path}
+                    exact={route.exact}
+                    component={route.main}
+                  />
+                ))}
+              </div>
             </div>
-        </Router>
-        )
-    }
-};
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from '../assets/logo.svg';
 
 import Github from '../assets/github-logo.png';
@@ -9,29 +9,69 @@ import Blog from '../assets/blog.png';
 import '../App.css';
 
 export default class Sidebar extends Component {
-    render() {
-        return ( 
-            <div className="sidebar"> 
-                <table className="table-markee">
-                    <tbody>
-                        <tr>
-                            <td className="portfolio-id"><h2 className="sidebar-title"> Jarrett Bonner </h2></td>
-                            <td className="portfolio-id"><img src={logo} className="portfolio-logo" alt="logo"/></td>
-                        </tr>                    
-                    </tbody> 
-                </table>
+  render() {
+    return (
+      <div className="sidebar">
+        <table className="table-markee">
+          <tbody>
+            <tr>
+              <td className="portfolio-id">
+                <h2 className="sidebar-title"> Jarrett Bonner </h2>
+              </td>
+              <td className="portfolio-id">
+                <img src={logo} className="portfolio-logo" alt="logo" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
-                <table className="table-quick-links">
-                    <tbody>
-                        <tr>
-                            <td className="quick-links"><a href="https://github.com/Jzbonner" data-toggle="tooltip" data-placement="bottom" title="Github"><img src={Github} className="quick-links-img" alt="github"/></a></td>
-                            <td className="quick-links"><a href="https://www.linkedin.com/in/jarrett-bonner-28048339/" data-toggle="tooltip" data-placement="bottom" title="LinkedIn"><img src={LinkedIn} className="quick-links-img" alt="linkedin"/></a></td>
-                            <td className="quick-links"><a href="https://medium.com/@jarrett.bonner" data-toggle="tooltip" data-placement="bottom" title="Medium"><img src={Medium} className="quick-links-img" alt="medium"/></a></td>
-                            <td className="quick-links"><a href="#" data-toggle="tooltip" data-placement="bottom" title="Blog Soon Come"><img src={Blog} className="quick-links-img" alt="blog"/></a></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
-}  
+        <table className="table-quick-links">
+          <tbody>
+            <tr>
+              <td className="quick-links">
+                <a
+                  href="https://github.com/Jzbonner"
+                  data-toggle="tooltip"
+                  data-placement="bottom"
+                  title="Github">
+                  <img src={Github} className="quick-links-img" alt="github" />
+                </a>
+              </td>
+              <td className="quick-links">
+                <a
+                  href="https://www.linkedin.com/in/jarrett-bonner-28048339/"
+                  data-toggle="tooltip"
+                  data-placement="bottom"
+                  title="LinkedIn">
+                  <img
+                    src={LinkedIn}
+                    className="quick-links-img"
+                    alt="linkedin"
+                  />
+                </a>
+              </td>
+              <td className="quick-links">
+                <a
+                  href="https://medium.com/@jarrett.bonner"
+                  data-toggle="tooltip"
+                  data-placement="bottom"
+                  title="Medium">
+                  <img src={Medium} className="quick-links-img" alt="medium" />
+                </a>
+              </td>
+              <td className="quick-links">
+                <a
+                  href="#"
+                  data-toggle="tooltip"
+                  data-placement="bottom"
+                  title="Blog Soon Come">
+                  <img src={Blog} className="quick-links-img" alt="blog" />
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
