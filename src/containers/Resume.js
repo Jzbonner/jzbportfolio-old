f6fd3b2f50867c6e07428b85dfd194216f6b0f53
Jzbonner <jarrett.bonner@gmail.com>
2020-01-27 17:00:31 -0500

    changes to multi-media

--- src/containers/Resume.js
+++ src/containers/Resume.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ResumePic from "../assets/sidebar-icons/keyboard.png";
+import GoogleDocsViewer from "react-google-docs-viewer";
 import "../App.css";
 
 {
@@ -13,22 +14,25 @@ export default class Resume extends Component {
     return (
         <div className="resume-section">
           <div className="row">
-            <div className="col-lg-2">
+            <div className="col-lg-2 side-icon-bg">
               <img className="portfolio-side-icon" src={ResumePic} />
             </div>
-            <div className="col-lg-3">
+            <div className="col-lg-9 description-bg">
               <ul className="resume-description">
-                <li>Resume</li>
                 <li>Github Spotlight</li>
                 <li>LinkedIn</li>
                 <li>Recent Blog Post</li>
-              </ul>
-            </div>
-            <div className="col-lg-4">
-              <ul className="resume-description">
-                <li>Placeholder</li>
                 <li>Book Reccomendation</li>
               </ul>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-12">
+              <GoogleDocsViewer 
+                width="600px"
+                height="780px"
+                fileUrl="https://docs.google.com/document/d/1VxIm4JczdkoOJcBylz5pvuIAzBPQv_zguVvN2OTfJB4/edit?usp=sharing"
+              />
             </div>
           </div>
         </div>
