da1893957ea8e299d29492566807b1cd862d9069
Jzbonner <jarrett.bonner@gmail.com>
2020-01-28 01:15:02 -0500

    changes to pdf viewer functionality

--- src/containers/Resume.js
+++ src/containers/Resume.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ResumePic from "../assets/sidebar-icons/keyboard.png";
-import GoogleDocsViewer from "react-google-docs-viewer";
+import  { Document, Page }  from "react-pdf"; 
 import "../App.css";
 
 {
@@ -27,13 +27,14 @@ export default class Resume extends Component {
             </div>
           </div>
           <div className="row">
-            <div className="col-lg-12">
-              <GoogleDocsViewer 
-                width="600px"
-                height="780px"
-                fileUrl="https://docs.google.com/document/d/1VxIm4JczdkoOJcBylz5pvuIAzBPQv_zguVvN2OTfJB4/edit?usp=sharing"
-              />
+            <div className="col-lg-3" />
+            <div className="col-lg-6 pdf-js">
+              <Document 
+                file="https://github.com/Jzbonner/jzb-co/blob/master/multi-media/resume.pdf"
+              >
+              </Document>
             </div>
+            <div className="col-lg-3" />
           </div>
         </div>
     );
